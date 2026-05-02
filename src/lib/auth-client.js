64fe82c99a83a4528a24619d807999e159377764
mehedyPUST <mehedy.pust@gmail.com>
2026-05-02 13:04:51 +0600

    protecte

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react";
 
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000/"
+    baseURL: "https://suncart-gamma.vercel.app/"
 })
 
 export const { signIn, signUp, signOut, useSession } = createAuthClient()
